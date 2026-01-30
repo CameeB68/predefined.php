174dc55c57031f179632d8edbadd8b414e9d7a0a
CameeB68 <camee.bunescu@cvusdk12.net>
2026-01-29 22:10:54 -0700

    Add files via upload

---
+++ predefined.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Predefined Variables</title>
+</head>
+<body>
+
+<pre>
+<?php
+print_r($_SERVER);
+?>
+</pre>
+
+</body>
+</html>
